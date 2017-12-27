a11cddcf6bf4eb4caef2c272ac735d471f6e7523
Nuli <giarsyani.nuli@gmail.com>
2017-12-27 15:52:27 +0700

    update admin

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -19,7 +19,67 @@ class AdminSeeder extends Seeder
 				'email' => 'admin@example.com',
 				'password' => bcrypt('111111'),
 				'created_at' => date('Y-m-d H:i:s')
-        	]
+        	],
+            [
+                'name' => 'Abid',
+                'email' => 'abid@pondokit',
+                'password' => bcrypt('111111'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Amar',
+                'email' => 'amar@pondokit',
+                'password' => bcrypt('111111'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Bilal',
+                'email' => 'bilal@pondokit',
+                'password' => bcrypt('111111'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Usman',
+                'email' => 'usman@pondokit',
+                'password' => bcrypt('111111'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Tama',
+                'email' => 'tama@pondokit',
+                'password' => bcrypt('111111'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'ahmad',
+                'email' => 'ahmad@pondokit',
+                'password' => bcrypt('111111'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'yahya',
+                'email' => 'yahya@pondokit',
+                'password' => bcrypt('111111'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'farid',
+                'email' => 'farid@pondokit',
+                'password' => bcrypt('111111'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'dika',
+                'email' => 'dika@pondokit',
+                'password' => bcrypt('111111'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'zaki',
+                'email' => 'zaki@pondokit',
+                'password' => bcrypt('111111'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
         ]);
     }
 }
